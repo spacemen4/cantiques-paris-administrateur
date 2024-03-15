4965bdcabdd2712a67076e6772e1dfb533c770d9
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-03-15 19:43:03 +0000

    vcbcvbvcvcbvc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,22 @@
-// Header.js
-
 import React from "react";
 import { Flex, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom"; // Assuming you are using React Router
 
 const Header = () => {
   return (
-    <Flex as="header" align="center" justify="space-between" p="4">
-      <Link to="/"> {/* Assuming you have defined routes for Home, Buy, and Sell */}
-        <Button variant="ghost">Home</Button>
-      </Link>
-      <Link to="/buy">
-        <Button variant="ghost">Buy</Button>
-      </Link>
-      <Link to="/sell">
-        <Button variant="ghost">Sell</Button>
-      </Link>
+    <Flex as="header" width="100%" align="center" justify="center" p="4">
+      {/* Inner Flex container to group buttons and apply defined spacing */}
+      <Flex gap="2" align="center"> {/* Adjust the gap value as needed for your design */}
+        <Link to="/">
+          <Button variant="ghost">Home</Button>
+        </Link>
+        <Link to="/buy">
+          <Button variant="ghost">Buy</Button>
+        </Link>
+        <Link to="/sell">
+          <Button variant="ghost">Sell</Button>
+        </Link>
+      </Flex>
     </Flex>
   );
 };
