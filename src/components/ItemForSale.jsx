94c807f28dc8f51eab1b99940a1213b3fc650062
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-03-15 22:22:38 +0000

    fgggfddsffd

--- src/components/ItemForSale.jsx
+++ src/components/ItemForSale.jsx
@@ -16,9 +16,9 @@ const SaleItemCard = ({ imageUrl, title, price, daysLeft, likes }) => {
   return (
     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" bg={cardBg}>
       <Box position="relative">
-        <Image src={imageUrl} alt={title} borderRadius="lg" />
+        <Image src={imageUrl} alt={title} borderRadius="lg" borderTopRadius="lg" borderBottomRadius="0" />
 
-        <Box position="absolute" top="2" right="2" d="flex" alignItems="center">
+        <Box position="absolute" top="2" right="2" display="flex" alignItems="center">
           <IconButton
             aria-label={`Like ${title}`}
             icon={<FaHeart />}
