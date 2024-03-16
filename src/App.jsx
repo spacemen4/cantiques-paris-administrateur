6db1469b0a421d4d2514474d5345d0dfb81678b6
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-03-16 16:13:40 +0000

    gfgfgdddffddfd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"; // Import Navigate for redirection
 import { ChakraProvider } from "@chakra-ui/react";
 import HeaderTop from "./components/HeaderTop";
 import Header from "./components/Header";
 import ArtChoices from "./components/pages/ArtChoices";
-import IntérieurPage from "./components/pages/IntérieurPage"; // Import the IntérieurPage component
-import BijouxPage from "./components/pages/BijouxPage"; // Import the BijouxPage component
+import IntérieurPage from "./components/pages/IntérieurPage";
+import BijouxPage from "./components/pages/BijouxPage";
 import CollectionCard from "./components/CollectionCard";
 import ItemForSale from "./components/ItemForSale";
 import FooterComponent from "./components/FooterComponent";
@@ -18,9 +18,10 @@ const App = () => {
         <HeaderTop />
         <Header />
         <Routes>
+          <Route path="/" element={<Navigate to="/art" />} /> {/* Redirect root to /art */}
           <Route path="/art" element={<ArtChoices />} />
-          <Route path="/interieur" element={<IntérieurPage />} /> {/* Route for Intérieur page */}
-          <Route path="/bijoux" element={<BijouxPage />} /> {/* Route for Bijoux page */}
+          <Route path="/interieur" element={<IntérieurPage />} />
+          <Route path="/bijoux" element={<BijouxPage />} />
         </Routes>
         <CollectionCard />
         <ItemForSale />
