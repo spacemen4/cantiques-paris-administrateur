c59d7baf654ebab18304717ec07832d8491bf25d
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-03-17 12:33:58 +0000

    erererereeerr

--- src/components/CollectionCardPopulaire.jsx
+++ src/components/CollectionCardPopulaire.jsx
@@ -2,32 +2,62 @@ import React from 'react';
 import { Box, Heading, Image, Text, Link, Button, Grid, GridItem, Badge, Flex } from '@chakra-ui/react';
 
 const CollectionCardPopulaire = ({ title, images, date, itemCount, itemLink }) => {
-  const gridTemplateRows = "repeat(1, auto)"; // Adjusted to match the other component
+  const gridTemplateRows = "repeat(1, auto)";
   const gridTemplateColumns = "3fr 1fr";
 
   return (
     <Box borderRadius="lg" overflow="hidden">
       <Box>
         <Grid templateRows={gridTemplateRows} templateColumns={gridTemplateColumns} gap={2}>
-          <GridItem colSpan={1} rowSpan={3} position="relative"> {/* Adjusted rowSpan to match the other component */}
+          <GridItem colSpan={1} rowSpan={3} position="relative">
             <Image src={images[0].src} alt={images[0].alt} objectFit="cover" width="100%" height="100%" />
           </GridItem>
           {images.slice(1, 4).map((image, index) => (
             <GridItem key={index + 1} colSpan={1} rowSpan={1} position="relative">
-              <Image src={image.src} alt={image.alt} objectFit="cover" width="100%" height="100%" />
-              {image.counter && (
-                <Badge
-                  position="absolute"
-                  bottom="10px"
-                  right="10px"
-                  bg="red.500"
-                  color="white"
-                  borderRadius="full"
-                  px={2}
-                  py={1}
-                >
-                  +{image.counter}
-                </Badge>
+              {index === 2 ? (
+                <Box position="relative">
+                  <Image src={image.src} alt={image.alt} objectFit="cover" width="100%" height="100%" />
+                  {image.counter && (
+                    <Badge
+                      position="absolute"
+                      bottom="10px"
+                      right="10px"
+                      bg="red.500"
+                      color="white"
+                      borderRadius="full"
+                      px={2}
+                      py={1}
+                    >
+                      +{image.counter}
+                    </Badge>
+                  )}
+                  <Box
+                    position="absolute"
+                    bottom="0"
+                    right="0"
+                    width="100%"
+                    height="100%"
+                    bg="rgba(0,0,0,0.4)" // Transparent gray background
+                  />
+                </Box>
+              ) : (
+                <>
+                  <Image src={image.src} alt={image.alt} objectFit="cover" width="100%" height="100%" />
+                  {image.counter && (
+                    <Badge
+                      position="absolute"
+                      bottom="10px"
+                      right="10px"
+                      bg="red.500"
+                      color="white"
+                      borderRadius="full"
+                      px={2}
+                      py={1}
+                    >
+                      +{image.counter}
+                    </Badge>
+                  )}
+                </>
               )}
             </GridItem>
           ))}
