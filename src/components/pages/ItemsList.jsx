906be68f7a64244a571ea92f06ed29d881230769
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2024-03-18 18:06:53 +0000

    dddddddddddd

--- src/components/pages/ItemsList.jsx
+++ src/components/pages/ItemsList.jsx
@@ -29,7 +29,28 @@ const ItemsList = () => {
           <Text>Lot Number: {item.lot_number}</Text>
           <Text>Closing Time: {item.closing_time}</Text>
           <Text>Current Offer: {item.current_offer}</Text>
-          {/* Add other item details you want to display */}
+          <Text>No Reserve Price: {item.no_reserve_price ? "Yes" : "No"}</Text>
+          <Text>Estimated Gallery Value: {item.estimated_gallery_value}</Text>
+          <Text>Selected By: {item.selected_by}</Text>
+          <Text>Buyer Protection Fee: {item.buyer_protection_fee}</Text>
+          <Text>Payment Methods: {item.payment_methods.join(", ")}</Text>
+          <Text>Shipping to France: {item.shipping_to_france}</Text>
+          <Text>Shipping to Portugal: {item.shipping_to_portugal}</Text>
+          <Text>Seller Name: {item.seller_name}</Text>
+          <Text>Location: {item.location}</Text>
+          <Text>Member Since: {item.member_since}</Text>
+          <Text>Seller Ratings: {item.seller_ratings}</Text>
+          <Text>Item Description: {item.item_description}</Text>
+          <Text>Artist Biography: {item.artist_biography}</Text>
+          <Text>Artwork Technique: {item.artwork_technique}</Text>
+          <Text>Artwork Signature: {item.artwork_signature}</Text>
+          <Text>Artwork Year: {item.artwork_year}</Text>
+          <Text>Artwork Condition: {item.artwork_condition}</Text>
+          <Text>Artwork Dimensions: {item.artwork_dimensions}</Text>
+          <Text>Artwork Origin: {item.artwork_origin}</Text>
+          <Text>Artwork Period: {item.artwork_period}</Text>
+          <Text>Legal Information: {item.legal_information}</Text>
+          {/* Add other item details here */}
         </Box>
       ))}
     </Box>
