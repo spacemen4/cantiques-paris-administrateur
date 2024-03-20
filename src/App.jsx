b4181cff5dde1d8e8dc54d3e79b599a2dbbc066b
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2024-03-20 03:28:59 +0000

    dddddddddddddddddddddddddd

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,60 @@ const App = () => {
               },
             }}
             providers={[""]}
-
+            localization={{
+              variables: {
+                sign_in: {
+                  email_label: 'Adresse e-mail',
+                  password_label: 'Votre mot de passe',
+                  email_input_placeholder: 'Votre adresse e-mail',
+                  password_input_placeholder: 'Votre mot de passe',
+                  button_label: 'Se connecter',
+                  loading_button_label: 'Connexion en cours...',
+                  link_text: 'Vous avez déjà un compte? Connectez-vous',
+                },
+                sign_up: {
+                  email_label: 'Adresse e-mail',
+                  password_label: 'Créer un mot de passe',
+                  email_input_placeholder: 'Votre adresse e-mail',
+                  password_input_placeholder: 'Votre mot de passe',
+                  button_label: 'S\'inscrire',
+                  loading_button_label: 'Inscription en cours...',
+                  link_text: 'Vous n\'avez pas de compte? Inscrivez-vous',
+                },
+                magic_link: {
+                  email_input_placeholder: 'Votre adresse e-mail',
+                  button_label: 'Envoyer un e-mail avec un lien magique',
+                  loading_button_label: 'Envoi du lien magique en cours...',
+                  link_text: 'Envoyer un e-mail avec un lien magique',
+                  confirmation_text: 'Consultez votre e-mail pour le lien magique',
+                },
+                forgotten_password: {
+                  email_label: 'Adresse e-mail',
+                  password_label: 'Votre mot de passe',
+                  email_input_placeholder: 'Votre adresse e-mail',
+                  button_label: 'Envoyer des instructions de réinitialisation de mot de passe',
+                  loading_button_label: 'Envoi des instructions de réinitialisation...',
+                  link_text: 'Mot de passe oublié?',
+                  confirmation_text: 'Consultez votre e-mail pour le lien de réinitialisation de mot de passe',
+                },
+                update_password: {
+                  password_label: 'Nouveau mot de passe',
+                  password_input_placeholder: 'Votre nouveau mot de passe',
+                  button_label: 'Mettre à jour le mot de passe',
+                  loading_button_label: 'Mise à jour du mot de passe...',
+                  confirmation_text: 'Votre mot de passe a été mis à jour',
+                },
+                verify_otp: {
+                  email_input_placeholder: 'Votre adresse e-mail',
+                  phone_input_label: 'Numéro de téléphone',
+                  phone_input_placeholder: 'Votre numéro de téléphone',
+                  token_input_label: 'Jeton',
+                  token_input_placeholder: 'Votre jeton OTP',
+                  button_label: 'Vérifier le jeton',
+                  loading_button_label: "Vérification du jeton...",
+                },
+              },
+            }}
           />
         </div>
       </div>
